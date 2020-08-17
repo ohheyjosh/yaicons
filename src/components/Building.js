e99be98d7d7aa5b88ced60171336d501f7818ce3
Josh Barnett <oh@heyjo.sh>
2020-08-17 15:33:09 -0400

    React typos in props

--- src/components/Building.js
+++ src/components/Building.js
@@ -18,12 +18,12 @@ const Building = forwardRef(
       >
         <g
           fill="none"
-          fill-rule="evenodd"
+          fillRule="evenodd"
           stroke={color}
-          stroke-linejoin="round"
+          strokeLinejoin="round"
           transform="translate(4 8)"
         >
-          <line x2="88" y1="80" y2="80" stroke-linecap="round" />
+          <line x2="88" y1="80" y2="80" strokeLinecap="round" />
           <path d="M16,0 L72,0 C73.1045695,-4.1271253e-15 74,0.8954305 74,2 L74,80 L74,80 L14,80 L14,2 C14,0.8954305 14.8954305,2.02906125e-16 16,0 Z" />
           <rect width="16" height="20" x="36" y="60" />
           <rect width="8" height="8" x="24" y="9" />
